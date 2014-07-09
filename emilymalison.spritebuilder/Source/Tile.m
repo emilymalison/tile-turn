ff80e8a5453c1d0214d1aa1cea9a26efc0cf49f3
Emily Malison <emalison16@gmail.com>
2014-07-09 14:22:01 -0400

    Added dots to the tiles

--- emilymalison.spritebuilder/Source/Tile.m
+++ emilymalison.spritebuilder/Source/Tile.m
@@ -8,8 +8,76 @@
 
 #import "Tile.h"
 
+static const int TILE_SIZE=3;
 
-@implementation Tile
+@implementation Tile{
+    NSMutableArray *_dotArray;
+    NSMutableArray *_tileArray;
+    CGFloat _dotMarginHorizontal;
+    CGFloat _dotMarginVertical;
+    CGFloat _tileColumnWidth;
+    CGFloat _tileColumnHeight;
+}
+
+- (void)onEnter
+{
+    
+    [super onEnter];
+    
+    [self setUpTile];
+}
+
+-(void)setUpTile{
+    _tileColumnHeight=self.contentSize.height/3;
+    _tileColumnWidth=self.contentSize.width/3;
+    
+    
+    _dotMarginHorizontal=_tileColumnWidth/TILE_SIZE;
+    _dotMarginVertical=_tileColumnHeight/TILE_SIZE;
+    
+    float x=_dotMarginHorizontal;
+    float y=_dotMarginVertical;
+    
+    
+    for (int i=0; i<3; i++) {
+        x=_dotMarginHorizontal;
+        
+        for (int j=0; j<3; j++) {
+            int numberDot=arc4random()%3;
+            if (numberDot==0) {
+                CCSprite *dot=(CCSprite*)[CCBReader load:@"Dot1"];
+                [dot setScaleX:(((_tileColumnWidth)/dot.contentSize.width))/2];
+                [dot setScaleY:(((_tileColumnHeight)/dot.contentSize.height))/2];
+                [self addChild:dot];
+                dot.contentSize = CGSizeMake(_tileColumnWidth, _tileColumnHeight);
+                dot.position = ccp(x, y);
+            }
+            else if(numberDot==1){
+                CCSprite *dot=(CCSprite*)[CCBReader load: @"Dot2"];
+                [dot setScaleX:(((_tileColumnWidth)/dot.contentSize.width))/2];
+                [dot setScaleY:(((_tileColumnHeight)/dot.contentSize.height))/2];
+                [self addChild:dot];
+                dot.contentSize=CGSizeMake(_tileColumnWidth, _tileColumnHeight);
+                dot.position=ccp(x, y);
+            }
+            else if(numberDot==2){
+                CCSprite *dot=(CCSprite*)[CCBReader load: @"Dot3"];
+                [dot setScaleX:(((_tileColumnWidth)/dot.contentSize.width))/2];
+                [dot setScaleY:(((_tileColumnHeight)/dot.contentSize.height))/2];
+                [self addChild:dot];
+                dot.contentSize=CGSizeMake(_tileColumnWidth, _tileColumnHeight);
+                dot.position=ccp(x, y);
+            }
+            
+            x+=_tileColumnWidth;
+        }
+        y+=_tileColumnHeight;
+    }
+    
+}
+
+
+
 
 
 
